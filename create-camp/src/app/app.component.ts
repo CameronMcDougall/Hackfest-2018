d9e6889d0a077ae29fc8b27547c295b1723488e4
CameronMcDougall <cameronmcdougall97@yahoo.co.nz>
2018-08-25 14:51:38 +1200

    Search bar added

--- create-camp/src/app/app.component.ts
+++ create-camp/src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'app';
+  cities: string[] = ['Lowry Bay', 'Seatoun', 'Pipitea', 'Te Aro', 'Mirimar','Lyall Bay'] ;
+  
+  getSearchMatches(text: string){
+    var contains =[];
+    this.cities.forEach(element => {
+      if(this.cities.includes(text)){
+          contains.push(element);
+      }
+    });
+    return contains;
+  } 
+
+
+
 }
