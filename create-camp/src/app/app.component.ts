4650993c145d19f66f6c03ec12feeaf2a5860b5d
CameronMcDougall <cameronmcdougall97@yahoo.co.nz>
2018-08-26 15:50:50 +1200

    fixes

--- create-camp/src/app/app.component.ts
+++ create-camp/src/app/app.component.ts
@@ -9,44 +9,84 @@ import { data } from "./properties";
 })
 export class AppComponent {
   properties: Property[] = [];
+  displayProperties: Property[] = [];
   title: string = "app";
   maxPrice: number = 0;
   minPrice: number = 100000;
   displayMinPrice:number = 0;
   displayMaxPrice:number = 500;
   numOfRooms: number[]= [1,2,3,4,5]
-  amountOfRooms: number;
+  amountOfRooms: number = 1;
   price: number;
-  cities: string[] = [
-    "lowry bay",
-    "seatoun",
-    "pipitea",
-    "te aro",
-    "mirimar",
-    "lyall bay"
-  ];
+  selectedSuburb:string;
+  cities: string[] = [];
 
   changeMinSlider(value){
-    this.displayMaxPrice = value;
-   
+    this.displayMinPrice = value;
+   this.findProperties();
 
   }
   changeMaxSlider(value){
 
     this.displayMaxPrice = value;
-   
+    this.findProperties();
   }
   changeRooms(value){
+    
+    this.amountOfRooms = value;
+    this.findProperties();
+  }
+  print(){
+    console.log(this.selectedSuburb);
+  }
 
-    this.maxPrice = value;
-  
+  findProperties(){
+    this.displayProperties = []
+    this.properties.forEach(element => {
+      
+      let price: number = Number(element.price.substring(1,element.price.length));
+     
+
+      console.log("Element price: "+price );
+     console.log("min price: "+this.displayMinPrice );
+      console.log("max price: "+ this.displayMaxPrice);
+      console.log("max price from init: "+ this.maxPrice);
+      if(this.selectedSuburb == "All"&& element.numBedrooms >= this.amountOfRooms
+      && price <= this.displayMaxPrice && price >= this.displayMinPrice){
+        this.displayProperties.push(element);
+      }
+     else if(element.suburb == this.selectedSuburb 
+        && element.numBedrooms >= this.amountOfRooms
+        && price <= this.displayMaxPrice && price >= this.displayMinPrice
+        ){
+            this.displayProperties.push(element);
+        }
+      
+    });
+    console.log(this.displayProperties.length);
+  }
+  changedSuburb(){
+    this.findProperties();
   }
   constructor() {
     //console.log(data);
     this.parseData();
+    this.getCities();
+    this.displayProperties = this.properties;
   }
 
- 
+  getCities(){
+    this.cities.push("All");
+    this.properties.forEach(
+      element=>
+      {
+        if(!this.cities.includes(element.suburb)){
+          this.cities.push(element.suburb);
+        }
+      }
+    );
+   
+  }
   selectedOption: string;
   lat: number = -41.2440266;
   lng: number = 174.6214276;
